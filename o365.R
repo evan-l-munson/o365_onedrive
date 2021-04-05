0b9d4f168851a1d290d79dc694fda90a7f9f4813
Evan Munson <evan.l.munson@gmail.com>
2021-04-05 12:43:40 -0700

    downloaded and uploaded file

---
+++ o365.R
@@ -0,0 +1,31 @@
+
+# Libraries ---------------------------------------------------------------
+
+library(Microsoft365R)
+library(xlsx)
+
+
+# Explore -----------------------------------------------------------------
+
+# Connect to OneDrive
+elm_od <- Microsoft365R::get_personal_onedrive()
+
+# List files and folders
+elm_od$list_files("Projects")
+elm_od$list_files("Documents")
+elm_od$list_files("Documents/Munson Finances")
+
+# Download file
+elm_od$download_file("Documents/Munson Finances/Finance Analysis.xlsx")
+
+# Read in file
+finances <- readxl::read_xlsx(path = "Finance Analysis.xlsx")
+
+# create toy dataset
+toy <- sample(x = 1:5, size = 10, replace = TRUE)
+toy_df <- as.data.frame(toy)
+
+xlsx::write.xlsx(x = toy_df, file = "toy_df.xlsx")
+
+# Save toy_df.xlsx back to OneDrive
+elm_od$upload_file(src = "toy_df.xlsx", dest = "Projects/toy_df.xlsx")
