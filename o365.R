252687b7483e4bed4e6789f475e8e01f5e6fa8c8
Evan Munson <evan.l.munson@gmail.com>
2021-04-05 12:55:43 -0700

    adjusted the uploaded file

--- o365.R
+++ o365.R
@@ -4,7 +4,6 @@
 library(Microsoft365R)
 library(xlsx)
 
-
 # Explore -----------------------------------------------------------------
 
 # Connect to OneDrive
@@ -13,13 +12,13 @@ elm_od <- Microsoft365R::get_personal_onedrive()
 # List files and folders
 elm_od$list_files("Projects")
 elm_od$list_files("Documents")
-elm_od$list_files("Documents/Munson Finances")
+elm_od$list_files("Documents/Munson Stuff")
 
 # Download file
-elm_od$download_file("Documents/Munson Finances/Finance Analysis.xlsx")
+elm_od$download_file("Documents/Munson Stuff/Stuff.xlsx")
 
 # Read in file
-finances <- readxl::read_xlsx(path = "Finance Analysis.xlsx")
+stuff <- readxl::read_xlsx(path = "Stuff.xlsx")
 
 # create toy dataset
 toy <- sample(x = 1:5, size = 10, replace = TRUE)
